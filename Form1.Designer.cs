69bc10f309a12ea751b1691796865184ae4b1034
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-22 21:50:06 -0600

    Updated Form Design and Code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,18 +30,18 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.tboxNumGuests = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.gboxDestination = new System.Windows.Forms.GroupBox();
+            this.lboxDestination = new System.Windows.Forms.ListBox();
+            this.gboxDepartureDate = new System.Windows.Forms.GroupBox();
             this.radApril30 = new System.Windows.Forms.RadioButton();
             this.radMay07 = new System.Windows.Forms.RadioButton();
             this.radMay14 = new System.Windows.Forms.RadioButton();
             this.radMay21 = new System.Windows.Forms.RadioButton();
             this.radMay28 = new System.Windows.Forms.RadioButton();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.gboxHotel = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.tboxNumNights = new System.Windows.Forms.TextBox();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.gboxRentalCar = new System.Windows.Forms.GroupBox();
             this.radBasic = new System.Windows.Forms.RadioButton();
             this.radLuxury = new System.Windows.Forms.RadioButton();
             this.radNone = new System.Windows.Forms.RadioButton();
@@ -51,10 +51,10 @@
             this.lblOutTotalCost = new System.Windows.Forms.Label();
             this.btnCalcTotalCost = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
-            this.groupBox4.SuspendLayout();
+            this.gboxDestination.SuspendLayout();
+            this.gboxDepartureDate.SuspendLayout();
+            this.gboxHotel.SuspendLayout();
+            this.gboxRentalCar.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -75,51 +75,52 @@
             this.tboxNumGuests.Size = new System.Drawing.Size(114, 26);
             this.tboxNumGuests.TabIndex = 1;
             // 
-            // groupBox1
+            // gboxDestination
             // 
-            this.groupBox1.Controls.Add(this.listBox1);
-            this.groupBox1.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(38, 86);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(244, 128);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Destination";
+            this.gboxDestination.Controls.Add(this.lboxDestination);
+            this.gboxDestination.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gboxDestination.Location = new System.Drawing.Point(38, 86);
+            this.gboxDestination.Name = "gboxDestination";
+            this.gboxDestination.Size = new System.Drawing.Size(244, 128);
+            this.gboxDestination.TabIndex = 2;
+            this.gboxDestination.TabStop = false;
+            this.gboxDestination.Text = "Destination";
             // 
-            // listBox1
+            // lboxDestination
             // 
-            this.listBox1.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 19;
-            this.listBox1.Items.AddRange(new object[] {
-            "New York",
-            "Los Angeles",
-            "Miami",
-            "Chicago",
-            "San Fancisco"});
-            this.listBox1.Location = new System.Drawing.Point(6, 23);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(232, 99);
-            this.listBox1.TabIndex = 0;
+            this.lboxDestination.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lboxDestination.FormattingEnabled = true;
+            this.lboxDestination.ItemHeight = 19;
+            this.lboxDestination.Items.AddRange(new object[] {
+            "New York ($400)",
+            "Los Angeles ($500)",
+            "Miami ($450)",
+            "Chicago ($350)",
+            "San Francisco ($550)"});
+            this.lboxDestination.Location = new System.Drawing.Point(6, 23);
+            this.lboxDestination.Name = "lboxDestination";
+            this.lboxDestination.Size = new System.Drawing.Size(232, 99);
+            this.lboxDestination.TabIndex = 0;
             // 
-            // groupBox2
+            // gboxDepartureDate
             // 
-            this.groupBox2.Controls.Add(this.radMay28);
-            this.groupBox2.Controls.Add(this.radMay21);
-            this.groupBox2.Controls.Add(this.radMay14);
-            this.groupBox2.Controls.Add(this.radMay07);
-            this.groupBox2.Controls.Add(this.radApril30);
-            this.groupBox2.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(288, 86);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(200, 191);
-            this.groupBox2.TabIndex = 3;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Departure Date";
+            this.gboxDepartureDate.Controls.Add(this.radMay28);
+            this.gboxDepartureDate.Controls.Add(this.radMay21);
+            this.gboxDepartureDate.Controls.Add(this.radMay14);
+            this.gboxDepartureDate.Controls.Add(this.radMay07);
+            this.gboxDepartureDate.Controls.Add(this.radApril30);
+            this.gboxDepartureDate.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gboxDepartureDate.Location = new System.Drawing.Point(288, 86);
+            this.gboxDepartureDate.Name = "gboxDepartureDate";
+            this.gboxDepartureDate.Size = new System.Drawing.Size(200, 199);
+            this.gboxDepartureDate.TabIndex = 5;
+            this.gboxDepartureDate.TabStop = false;
+            this.gboxDepartureDate.Text = "Departure Date";
             // 
             // radApril30
             // 
             this.radApril30.AutoSize = true;
+            this.radApril30.Checked = true;
             this.radApril30.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radApril30.Location = new System.Drawing.Point(37, 34);
             this.radApril30.Name = "radApril30";
@@ -177,23 +178,23 @@
             this.radMay28.Text = "May 28";
             this.radMay28.UseVisualStyleBackColor = true;
             // 
-            // groupBox3
+            // gboxHotel
             // 
-            this.groupBox3.Controls.Add(this.tboxNumNights);
-            this.groupBox3.Controls.Add(this.label2);
-            this.groupBox3.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(38, 220);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(244, 57);
-            this.groupBox3.TabIndex = 4;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Hotel Information";
+            this.gboxHotel.Controls.Add(this.tboxNumNights);
+            this.gboxHotel.Controls.Add(this.label2);
+            this.gboxHotel.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gboxHotel.Location = new System.Drawing.Point(38, 220);
+            this.gboxHotel.Name = "gboxHotel";
+            this.gboxHotel.Size = new System.Drawing.Size(244, 65);
+            this.gboxHotel.TabIndex = 3;
+            this.gboxHotel.TabStop = false;
+            this.gboxHotel.Text = "Hotel ($150 per night)";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 28);
+            this.label2.Location = new System.Drawing.Point(6, 36);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(126, 19);
             this.label2.TabIndex = 0;
@@ -202,25 +203,25 @@
             // tboxNumNights
             // 
             this.tboxNumNights.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tboxNumNights.Location = new System.Drawing.Point(138, 25);
+            this.tboxNumNights.Location = new System.Drawing.Point(138, 33);
             this.tboxNumNights.Name = "tboxNumNights";
             this.tboxNumNights.Size = new System.Drawing.Size(100, 26);
-            this.tboxNumNights.TabIndex = 1;
+            this.tboxNumNights.TabIndex = 4;
             // 
-            // groupBox4
+            // gboxRentalCar
             // 
-            this.groupBox4.Controls.Add(this.tboxNumDays);
-            this.groupBox4.Controls.Add(this.label3);
-            this.groupBox4.Controls.Add(this.radNone);
-            this.groupBox4.Controls.Add(this.radLuxury);
-            this.groupBox4.Controls.Add(this.radBasic);
-            this.groupBox4.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox4.Location = new System.Drawing.Point(494, 86);
-            this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(245, 191);
-            this.groupBox4.TabIndex = 2;
-            this.groupBox4.TabStop = false;
-            this.groupBox4.Text = "Rental Car Information";
+            this.gboxRentalCar.Controls.Add(this.tboxNumDays);
+            this.gboxRentalCar.Controls.Add(this.label3);
+            this.gboxRentalCar.Controls.Add(this.radNone);
+            this.gboxRentalCar.Controls.Add(this.radLuxury);
+            this.gboxRentalCar.Controls.Add(this.radBasic);
+            this.gboxRentalCar.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gboxRentalCar.Location = new System.Drawing.Point(494, 86);
+            this.gboxRentalCar.Name = "gboxRentalCar";
+            this.gboxRentalCar.Size = new System.Drawing.Size(245, 199);
+            this.gboxRentalCar.TabIndex = 6;
+            this.gboxRentalCar.TabStop = false;
+            this.gboxRentalCar.Text = "Rental Car Information";
             // 
             // radBasic
             // 
@@ -228,10 +229,10 @@
             this.radBasic.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radBasic.Location = new System.Drawing.Point(18, 49);
             this.radBasic.Name = "radBasic";
-            this.radBasic.Size = new System.Drawing.Size(72, 23);
+            this.radBasic.Size = new System.Drawing.Size(198, 23);
             this.radBasic.TabIndex = 0;
             this.radBasic.TabStop = true;
-            this.radBasic.Text = "Basic";
+            this.radBasic.Text = "Basic ($50 per day)";
             this.radBasic.UseVisualStyleBackColor = true;
             // 
             // radLuxury
@@ -240,10 +241,10 @@
             this.radLuxury.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radLuxury.Location = new System.Drawing.Point(18, 78);
             this.radLuxury.Name = "radLuxury";
-            this.radLuxury.Size = new System.Drawing.Size(81, 23);
+            this.radLuxury.Size = new System.Drawing.Size(216, 23);
             this.radLuxury.TabIndex = 1;
             this.radLuxury.TabStop = true;
-            this.radLuxury.Text = "Luxury";
+            this.radLuxury.Text = "Luxury ($100 per day)";
             this.radLuxury.UseVisualStyleBackColor = true;
             // 
             // radNone
@@ -262,7 +263,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(14, 162);
+            this.label3.Location = new System.Drawing.Point(14, 170);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(108, 19);
             this.label3.TabIndex = 3;
@@ -271,10 +272,10 @@
             // tboxNumDays
             // 
             this.tboxNumDays.Font = new System.Drawing.Font("NanumGothicCoding", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tboxNumDays.Location = new System.Drawing.Point(128, 159);
+            this.tboxNumDays.Location = new System.Drawing.Point(128, 167);
             this.tboxNumDays.Name = "tboxNumDays";
             this.tboxNumDays.Size = new System.Drawing.Size(111, 26);
-            this.tboxNumDays.TabIndex = 4;
+            this.tboxNumDays.TabIndex = 7;
             // 
             // label4
             // 
@@ -302,9 +303,10 @@
             this.btnCalcTotalCost.Location = new System.Drawing.Point(38, 361);
             this.btnCalcTotalCost.Name = "btnCalcTotalCost";
             this.btnCalcTotalCost.Size = new System.Drawing.Size(219, 59);
-            this.btnCalcTotalCost.TabIndex = 7;
+            this.btnCalcTotalCost.TabIndex = 8;
             this.btnCalcTotalCost.Text = "Calculate Total Cost";
             this.btnCalcTotalCost.UseVisualStyleBackColor = true;
+            this.btnCalcTotalCost.Click += new System.EventHandler(this.btnCalcTotalCost_Click);
             // 
             // btnExit
             // 
@@ -312,9 +314,10 @@
             this.btnExit.Location = new System.Drawing.Point(263, 361);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(100, 59);
-            this.btnExit.TabIndex = 8;
+            this.btnExit.TabIndex = 9;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // frmTravelbookingSystem
             // 
@@ -325,21 +328,21 @@
             this.Controls.Add(this.btnCalcTotalCost);
             this.Controls.Add(this.lblOutTotalCost);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.groupBox4);
-            this.Controls.Add(this.groupBox3);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gboxRentalCar);
+            this.Controls.Add(this.gboxHotel);
+            this.Controls.Add(this.gboxDepartureDate);
+            this.Controls.Add(this.gboxDestination);
             this.Controls.Add(this.tboxNumGuests);
             this.Controls.Add(this.label1);
             this.Name = "frmTravelbookingSystem";
             this.Text = "Travel Booking System";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
-            this.groupBox4.ResumeLayout(false);
-            this.groupBox4.PerformLayout();
+            this.gboxDestination.ResumeLayout(false);
+            this.gboxDepartureDate.ResumeLayout(false);
+            this.gboxDepartureDate.PerformLayout();
+            this.gboxHotel.ResumeLayout(false);
+            this.gboxHotel.PerformLayout();
+            this.gboxRentalCar.ResumeLayout(false);
+            this.gboxRentalCar.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -349,18 +352,18 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tboxNumGuests;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox gboxDestination;
+        private System.Windows.Forms.ListBox lboxDestination;
+        private System.Windows.Forms.GroupBox gboxDepartureDate;
         private System.Windows.Forms.RadioButton radMay28;
         private System.Windows.Forms.RadioButton radMay21;
         private System.Windows.Forms.RadioButton radMay14;
         private System.Windows.Forms.RadioButton radMay07;
         private System.Windows.Forms.RadioButton radApril30;
-        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox gboxHotel;
         private System.Windows.Forms.TextBox tboxNumNights;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox gboxRentalCar;
         private System.Windows.Forms.RadioButton radNone;
         private System.Windows.Forms.RadioButton radLuxury;
         private System.Windows.Forms.RadioButton radBasic;
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,84 @@ namespace Travel_Booking_System
 {
     public partial class frmTravelbookingSystem : Form
     {
+        const double HOTEL_COST_PER_NIGHT = 150;
+        const double CAR_LUXURY = 100;
+        const double CAR_BASIC = 50;
+        
+        private double airFareCost;
+        private double hotelCost;
+        private double carRentalCost;
+        private double totalCost;
         public frmTravelbookingSystem()
         {
             InitializeComponent();
+        }
+
+        private void btnCalcTotalCost_Click(object sender, EventArgs e)
+        {
+            int numGuests = int.Parse(tboxNumGuests.Text);
+            string destination;
+
+            if(lboxDestination.SelectedIndex != -1)
+            {
+                destination = lboxDestination.SelectedItem.ToString();
+
+                if(destination == "New York ($400)")
+                {
+                    airFareCost = 400;
+                }
+                else if(destination == "Los Angeles ($500)")
+                {
+                    airFareCost = 500;
+                }
+                else if(destination == "Miami ($450)")
+                {
+                    airFareCost = 450;
+                }
+                else if(destination == "Chicago ($350)")
+                {
+                    airFareCost = 350;
+                }
+                else if(destination == "San Francisco ($550)")
+                {
+                    airFareCost = 550;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a destination.");
+            }
+
+            int numDays = int.Parse(tboxNumDays.Text);
+
+            if(radBasic.Checked)
+            {
+                carRentalCost = numDays * CAR_BASIC;
+            }
+
+            if(radLuxury.Checked)
+            {
+                carRentalCost = numDays * CAR_LUXURY;
+            }
+
+            if(radNone.Checked)
+            {
+                carRentalCost = numDays * 0;
+            }
+
+            int numNights = int.Parse(tboxNumNights.Text);
+
+            hotelCost = numNights * HOTEL_COST_PER_NIGHT;
+
+            totalCost = (airFareCost + carRentalCost + hotelCost) * numGuests;
+
+            lblOutTotalCost.Text = totalCost.ToString("c2");
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
